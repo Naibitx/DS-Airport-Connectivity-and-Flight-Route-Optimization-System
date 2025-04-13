695e721bffb4b3723cc85c4abb43dd1983591923
Mastermou8 <76893261+Mastermou8@users.noreply.github.com>
2025-04-13 16:08:51 -0400

    AirlineGraph cpp and h file uploaded
    AirlineGraph will calculate the cheapest rout from point A to point B.

---
+++ airlineGraph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maste on 4/13/2025.
+//
+
+#include "airlineGraph.h"
---
+++ airlineGraph.h
@@ -0,0 +1,16 @@
+//
+// Created by maste on 4/13/2025.
+//
+
+#ifndef AIRLINEGRAPH_H
+#define AIRLINEGRAPH_H
+
+
+
+class airlineGraph {
+
+};
+
+
+
+#endif //AIRLINEGRAPH_H
