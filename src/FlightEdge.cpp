e5905a6cbe3b6aa662d1663eea99f9e052cfb9d4
DaUneasyBat <151479212+DaUneasyBat@users.noreply.github.com>
2025-04-19 18:07:30 -0400

    Update FlightEdge.cpp

--- src/FlightEdge.cpp
+++ src/FlightEdge.cpp
@@ -1,1 +1,9 @@
+#include "FlightEdge.h"
 
+//  create the flight edges with origin, destination, distance, and cost
+
+FlightEdge::FlightEdge(const std::string origin, std::string dest, int dist, int cost){
+  this->origin = origin;
+  this->destination = dest;
+  this->distance = dist;
+  this->cost = cost;
