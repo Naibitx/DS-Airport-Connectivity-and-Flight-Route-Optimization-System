68f813fb18d16614882a205b10973b45b2940a54
Naibitx <naibys.alzugarayofficial@gmail.com>
2025-04-22 22:49:14 -0400

    changed main

--- Airport/main.cpp
+++ Airport/main.cpp
@@ -111,7 +111,7 @@ int main() {
     
     // Read CSV file once at startup
     cout << "Loading airports.csv..." << endl;
-    graph.readCSV("airports.csv");
+    graph.readCSV("airports.txt");
     
     while (true) {
         displayMenu();
